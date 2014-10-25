2ad3d6d95106b3a99a67dbdb7cd040f686e34376
Fiona Liao <fl2213@ic.ac.uk>
2014-10-25 15:01:29 +0100

    Did ingredients view

--- client/android/app/src/main/java/com/food4thought/food4thought/ui/view/food4thoughtapp/ingredients/IngredientsView.java
+++ client/android/app/src/main/java/com/food4thought/food4thought/ui/view/food4thoughtapp/ingredients/IngredientsView.java
@@ -20,5 +20,17 @@ public class IngredientsView extends LinearLayout {
     public IngredientsView(Context context) {
         super(context);
         setBackgroundColor(Color.BLUE);
+
+        friendlyTextView = new FriendlyTextView(context);
+        ingredientSearchView = new IngredientSearchView(context);
+        ingredientOptionsView = new IngredientOptionsView(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        friendlyTextView.setLayoutParams(new LayoutParams(right, (int) (0.30 * bottom)));
+        ingredientSearchView.setLayoutParams(new LayoutParams(right, (int) (0.40 * bottom)));
+        ingredientOptionsView.setLayoutParams(new LayoutParams(right, (int) (0.30 * bottom)));
     }
 }
